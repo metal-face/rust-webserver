92048f470eb9990bf07b5a3dc7a662c20f7de976
metal-face <captain@pirateradio.boats>
2023-01-01 18:24:21 -0500

    project refactored
    rather than having duplicate code, a tuple is utilized to return relevant data depending on the result of the boolean operation. This reduces the amount of repetition.

--- src/main.rs
+++ src/main.rs
@@ -18,17 +18,19 @@ fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    if request_line == "GET / HTTP/1.1" {
-        let status_line = "HTTP/1.1 200 OK";
-        let contents = fs::read_to_string("src\\hello.html").unwrap();
-        let length = contents.len();
-
-        let res = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-
-        stream.write_all(res.as_bytes()).unwrap();
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "src\\hello.html")
     } else {
-        let status_line = "HTTP/1.1 404 NOT FOUND";
-        let contents = fs::read_to_string("src\\404.html")
-    }
+        ("HTTP/1.1 404 NOT FOUND", "src\\404.html")
+    };
+
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+
+    let res = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+
+    stream.write_all(res.as_bytes()).unwrap();
 
 }
